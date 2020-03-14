1f1710ee327efa6b7132e930e69a5630c9762ae4
jaquelinebonoto <39954954+jaquelinebonoto@users.noreply.github.com>
2020-03-14 18:31:41 -0300

    Testes simples com laços e condicionais

---
+++ js/condicionais.js
@@ -0,0 +1,18 @@
+var idade = prompt("qual sua idade?")
+
+if(idade >= 18){
+    console.log("Maior de idade")
+}else{
+    alert("Menor de idade")
+}
+
+var count = 0
+while (count < 5) {
+    console.log(count)
+    count++
+};
+
+
+for(var hora= 0; hora <=5; hora++ ){
+    console.log(hora)
+}
