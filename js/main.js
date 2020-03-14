bccf92e0aee8a486959f36cfe42ac4de6d9d24e8
jaquelinebonoto <39954954+jaquelinebonoto@users.noreply.github.com>
2020-03-14 18:20:23 -0300

    Testes com array e json

---
+++ js/main.js
@@ -0,0 +1,20 @@
+console.log("casa")
+
+var lista = ["maçã", "pera", "laranja"]
+console.log(lista)
+lista.push("uva")
+console.log(lista)
+lista.pop() //remove o ultimo elemento do array
+console.log(lista)
+
+console.log(lista.toString())
+console.log(lista.join(" - "))
+
+
+var fruta = {nome: "maça", cor: "vermelho"}
+console.log(fruta)
+console.log(fruta.cor)
+
+var pessoas = [{nome:"Jaque", idade:31}, {nome: "Mariah", idade:5}]
+console.log(pessoas)
+console.log(pessoas[1].nome)
